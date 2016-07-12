3bc3f0c9198378e939452e74ca4dded414e420a5
Abhi Pandey <abhipandey@Abhis-MacBook-Air.local>
2016-07-11 23:28:44 -0400

    getRecruitmentStatus() works now

--- ctgovparser.py
+++ ctgovparser.py
@@ -5,8 +5,8 @@ describing a 'disease' (e.g. "breast cancer"). That raw input
 will be fed into a relative url, which will be concatenated to
 a base url for clinicaltrials.gov. Using that URL, the program
 will search for clinical trials for the user-defined 'disease'.
-The program will then find all clinical trial results, and: 
-1) create a list of clinical trials PER PAGE of results
+The program will then iterate through PAGES of clinical trial search results, and: 
+1) create a list of clinical trials
 2) iterate through the list to find the trial's 'recruiting status' 
 3) store each 'recruiting status' in a dedicated list
 4) navigate to the next page of results and repeate steps 1-3. 
@@ -22,9 +22,7 @@ go through each url in ct_list and:
 {status: "Recruiting", "Unknown", "Active, not recruiting", "Completed", "Recruiting",
 "Not yet recruiting"}
 
-Because each search results page returns 20 results, 
-ct_list will only contain 20 objects. After iterating through 
-the ct_list, the program must navigate to the next page of 
+Because each search results page returns 20 results, the program must navigate to the next page of 
 results. Below is the relative URL to get to the next page. 
 <a title="Show next page of results" href="/ct2/results?term=breast+cancer&amp;pg=2">Next Page (21-40)</a>
 1) Find and parse out thet relative URL. 
@@ -36,8 +34,6 @@ import requests
 from bs4 import BeautifulSoup
 import re 
 
-#Remaining tasks: define the function. 
-#Problem: inserting value of "disease" into the relative URL. 
 def getRecruitmentStatus():
 	term = raw_input('Please type a disease name: ')
 	base_url = "https://clinicaltrials.gov"
@@ -50,12 +46,11 @@ def getRecruitmentStatus():
 			ct_list.append(base_url + x['href'])
 		else: 
 			continue
-	#return ct_list
 	recruiting = {}
 	not_recruiting = {}
 	unknown_recruiting_status = {}
 	for trial in ct_list:
-		print trial
+		#print trial
 		trial_url = requests.get(trial).text
 		trial_soup = BeautifulSoup(trial_url, 'lxml')
 		if trial_soup.find('div', 'not-recruiting-status'):
@@ -66,20 +61,42 @@ def getRecruitmentStatus():
 			unknown_recruiting_status[trial] = str(trial_soup.find('div', 'unknown-recruiting-status').contents[0]).rstrip()
 		else: 
 			print "no recruitment text found"
-	#return not_recruiting
-	#return recruiting
-	#return unknown_recruiting_status
+	while brca_soup.find_all(title=re.compile('Show next page of results')): 
+		next_page_links_list = brca_soup.find_all(title=re.compile('Show next page of results'))
+		next_page_href = next_page_links_list[0]['href']
+		print next_page_href
+		next_page_url = base_url + next_page_href
+		print next_page_url
+		brca_xml = requests.get(next_page_url).text
+		brca_soup = BeautifulSoup(brca_xml, 'lxml')
+		a_tags = brca_soup.find_all('a')
+		ct_list = list()
+		for x in a_tags: 
+			if re.search('NCT', x['href']):
+				ct_list.append(base_url + x['href'])
+			else: 
+				continue
+				#return ct_list
+		for trial in ct_list:
+			trial_url = requests.get(trial).text
+			trial_soup = BeautifulSoup(trial_url, 'lxml')
+			if trial_soup.find('div', 'not-recruiting-status'):
+				not_recruiting[trial] = str(trial_soup.find('div', 'not-recruiting-status').contents[0]).rstrip()
+			elif trial_soup.find('div', 'recruiting-status'):
+				recruiting[trial] = str(trial_soup.find('div', 'recruiting-status').contents[0]).rstrip()
+			elif trial_soup.find('div', 'unknown-recruiting-status'):
+				unknown_recruiting_status[trial] = str(trial_soup.find('div', 'unknown-recruiting-status').contents[0]).rstrip()
+			else: 
+				print "no recruitment text found"
 
 	print "There are", len(recruiting), "trials that are recruiting currently!"
 	print "There are", len(not_recruiting), "trials that are NOT recuriting"
 	print "For", len(unknown_recruiting_status), "trials, the recruiting status is unknown"
+	return recruiting
+	return not_recruiting
+	return unknown_recruiting_status
 
 getRecruitmentStatus()
 
-"""
-next_page_links_list = brca_soup.find_all(title=re.compile('Show next page of results'))
-next_page_href = next_page_links_list[0]['href']
-next_page_url = base_url + next_page_href
-brca_xml = requests.get(next_page_url)
-print type(brca_xml)
-"""
+
+#need to look up Object Relational Model on Github. 
